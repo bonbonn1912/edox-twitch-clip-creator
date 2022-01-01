a2c911c97be1be2a375849383e3b10943cc790d3
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-01-01 17:43:35 +0100

    final

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5432;
 
 const routes = require('./routes.js');
 
